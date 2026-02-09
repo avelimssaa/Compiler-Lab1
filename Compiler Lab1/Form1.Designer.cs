922ddec594a41b388cc90e617db10c158b68c54c
Вадим Зенцов <vadimzentsov65@gmail.com>
2026-02-09 13:34:22 +0700

    Добавьте файлы проекта.

---
+++ Compiler Lab1/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Compiler_Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            toolStrip1 = new ToolStrip();
+            toolStrip2 = new ToolStrip();
+            richTextBox1 = new RichTextBox();
+            richTextBox2 = new RichTextBox();
+            toolStripButton1 = new ToolStripButton();
+            toolStripButton2 = new ToolStripButton();
+            toolStripButton3 = new ToolStripButton();
+            toolStripButton4 = new ToolStripButton();
+            toolStripButton5 = new ToolStripButton();
+            toolStripButton6 = new ToolStripButton();
+            toolStripButton7 = new ToolStripButton();
+            toolStripButton8 = new ToolStripButton();
+            toolStripButton9 = new ToolStripButton();
+            toolStripButton10 = new ToolStripButton();
+            toolStripButton11 = new ToolStripButton();
+            toolStripButton12 = new ToolStripButton();
+            toolStripButton13 = new ToolStripButton();
+            toolStripButton14 = new ToolStripButton();
+            toolStripButton15 = new ToolStripButton();
+            toolStripButton16 = new ToolStripButton();
+            toolStrip1.SuspendLayout();
+            toolStrip2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.ImageScalingSize = new Size(20, 20);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton1, toolStripButton2, toolStripButton3, toolStripButton4, toolStripButton5 });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(884, 27);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStrip2
+            // 
+            toolStrip2.ImageScalingSize = new Size(30, 30);
+            toolStrip2.Items.AddRange(new ToolStripItem[] { toolStripButton6, toolStripButton7, toolStripButton8, toolStripButton9, toolStripButton10, toolStripButton11, toolStripButton12, toolStripButton13, toolStripButton14, toolStripButton15, toolStripButton16 });
+            toolStrip2.Location = new Point(0, 27);
+            toolStrip2.Name = "toolStrip2";
+            toolStrip2.Size = new Size(884, 37);
+            toolStrip2.TabIndex = 1;
+            toolStrip2.Text = "toolStrip2";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(12, 71);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(860, 231);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(12, 308);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(860, 240);
+            richTextBox2.TabIndex = 3;
+            richTextBox2.Text = "";
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            toolStripButton1.Image = (Image)resources.GetObject("toolStripButton1.Image");
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(49, 24);
+            toolStripButton1.Text = "Файл";
+            // 
+            // toolStripButton2
+            // 
+            toolStripButton2.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            toolStripButton2.Image = (Image)resources.GetObject("toolStripButton2.Image");
+            toolStripButton2.ImageTransparentColor = Color.Magenta;
+            toolStripButton2.Name = "toolStripButton2";
+            toolStripButton2.Size = new Size(64, 24);
+            toolStripButton2.Text = "Правка";
+            // 
+            // toolStripButton3
+            // 
+            toolStripButton3.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            toolStripButton3.Image = (Image)resources.GetObject("toolStripButton3.Image");
+            toolStripButton3.ImageTransparentColor = Color.Magenta;
+            toolStripButton3.Name = "toolStripButton3";
+            toolStripButton3.Size = new Size(49, 24);
+            toolStripButton3.Text = "Текст";
+            // 
+            // toolStripButton4
+            // 
+            toolStripButton4.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            toolStripButton4.Image = (Image)resources.GetObject("toolStripButton4.Image");
+            toolStripButton4.ImageTransparentColor = Color.Magenta;
+            toolStripButton4.Name = "toolStripButton4";
+            toolStripButton4.Size = new Size(45, 24);
+            toolStripButton4.Text = "Пуск";
+            // 
+            // toolStripButton5
+            // 
+            toolStripButton5.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            toolStripButton5.Image = (Image)resources.GetObject("toolStripButton5.Image");
+            toolStripButton5.ImageTransparentColor = Color.Magenta;
+            toolStripButton5.Name = "toolStripButton5";
+            toolStripButton5.Size = new Size(71, 24);
+            toolStripButton5.Text = "Справка";
+            // 
+            // toolStripButton6
+            // 
+            toolStripButton6.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton6.Image = (Image)resources.GetObject("toolStripButton6.Image");
+            toolStripButton6.ImageTransparentColor = Color.Magenta;
+            toolStripButton6.Name = "toolStripButton6";
+            toolStripButton6.Size = new Size(34, 34);
+            toolStripButton6.Text = "toolStripButton6";
+            // 
+            // toolStripButton7
+            // 
+            toolStripButton7.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton7.Image = (Image)resources.GetObject("toolStripButton7.Image");
+            toolStripButton7.ImageTransparentColor = Color.Magenta;
+            toolStripButton7.Name = "toolStripButton7";
+            toolStripButton7.Size = new Size(34, 34);
+            toolStripButton7.Text = "toolStripButton7";
+            // 
+            // toolStripButton8
+            // 
+            toolStripButton8.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton8.Image = (Image)resources.GetObject("toolStripButton8.Image");
+            toolStripButton8.ImageTransparentColor = Color.Magenta;
+            toolStripButton8.Name = "toolStripButton8";
+            toolStripButton8.Size = new Size(34, 34);
+            toolStripButton8.Text = "toolStripButton8";
+            // 
+            // toolStripButton9
+            // 
+            toolStripButton9.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton9.Image = (Image)resources.GetObject("toolStripButton9.Image");
+            toolStripButton9.ImageTransparentColor = Color.Magenta;
+            toolStripButton9.Name = "toolStripButton9";
+            toolStripButton9.Size = new Size(34, 34);
+            toolStripButton9.Text = "toolStripButton9";
+            // 
+            // toolStripButton10
+            // 
+            toolStripButton10.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton10.Image = (Image)resources.GetObject("toolStripButton10.Image");
+            toolStripButton10.ImageTransparentColor = Color.Magenta;
+            toolStripButton10.Name = "toolStripButton10";
+            toolStripButton10.Size = new Size(34, 34);
+            toolStripButton10.Text = "toolStripButton10";
+            // 
+            // toolStripButton11
+            // 
+            toolStripButton11.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton11.Image = (Image)resources.GetObject("toolStripButton11.Image");
+            toolStripButton11.ImageTransparentColor = Color.Magenta;
+            toolStripButton11.Name = "toolStripButton11";
+            toolStripButton11.Size = new Size(34, 34);
+            toolStripButton11.Text = "toolStripButton11";
+            // 
+            // toolStripButton12
+            // 
+            toolStripButton12.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton12.Image = (Image)resources.GetObject("toolStripButton12.Image");
+            toolStripButton12.ImageTransparentColor = Color.Magenta;
+            toolStripButton12.Name = "toolStripButton12";
+            toolStripButton12.Size = new Size(34, 34);
+            toolStripButton12.Text = "toolStripButton12";
+            // 
+            // toolStripButton13
+            // 
+            toolStripButton13.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton13.Image = (Image)resources.GetObject("toolStripButton13.Image");
+            toolStripButton13.ImageTransparentColor = Color.Magenta;
+            toolStripButton13.Name = "toolStripButton13";
+            toolStripButton13.Size = new Size(34, 34);
+            toolStripButton13.Text = "toolStripButton13";
+            // 
+            // toolStripButton14
+            // 
+            toolStripButton14.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton14.Image = (Image)resources.GetObject("toolStripButton14.Image");
+            toolStripButton14.ImageTransparentColor = Color.Magenta;
+            toolStripButton14.Name = "toolStripButton14";
+            toolStripButton14.Size = new Size(34, 34);
+            toolStripButton14.Text = "toolStripButton14";
+            // 
+            // toolStripButton15
+            // 
+            toolStripButton15.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton15.Image = (Image)resources.GetObject("toolStripButton15.Image");
+            toolStripButton15.ImageTransparentColor = Color.Magenta;
+            toolStripButton15.Name = "toolStripButton15";
+            toolStripButton15.Size = new Size(34, 34);
+            toolStripButton15.Text = "toolStripButton15";
+            // 
+            // toolStripButton16
+            // 
+            toolStripButton16.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton16.Image = (Image)resources.GetObject("toolStripButton16.Image");
+            toolStripButton16.ImageTransparentColor = Color.Magenta;
+            toolStripButton16.Name = "toolStripButton16";
+            toolStripButton16.Size = new Size(34, 34);
+            toolStripButton16.Text = "toolStripButton16";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(884, 560);
+            Controls.Add(richTextBox2);
+            Controls.Add(richTextBox1);
+            Controls.Add(toolStrip2);
+            Controls.Add(toolStrip1);
+            Name = "Form1";
+            Text = "Compiler";
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            toolStrip2.ResumeLayout(false);
+            toolStrip2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButton1;
+        private ToolStrip toolStrip2;
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private ToolStripButton toolStripButton2;
+        private ToolStripButton toolStripButton3;
+        private ToolStripButton toolStripButton4;
+        private ToolStripButton toolStripButton5;
+        private ToolStripButton toolStripButton6;
+        private ToolStripButton toolStripButton7;
+        private ToolStripButton toolStripButton8;
+        private ToolStripButton toolStripButton9;
+        private ToolStripButton toolStripButton10;
+        private ToolStripButton toolStripButton11;
+        private ToolStripButton toolStripButton12;
+        private ToolStripButton toolStripButton13;
+        private ToolStripButton toolStripButton14;
+        private ToolStripButton toolStripButton15;
+        private ToolStripButton toolStripButton16;
+    }
+}
